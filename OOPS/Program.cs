52442748ef11e674eae82f56ad7a8f4c923f3bb7
Maheshwaran <71149425+MMahesh9695@users.noreply.github.com>
2021-03-06 00:25:47 +0530

    Add files via upload

---
+++ OOPS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPS.Factory;
+using OOPS.modal;
+
+namespace OOPS
+{
+    class Program
+    {        
+        //Factory Pattern : Different Implementation objects of same type
+        static void Main(string[] args)
+        {
+            string Type = Console.ReadLine();
+            ObjFactory fac = new ObjFactory();
+            IReceipt receipt = fac.getObject(Type);
+            string name = receipt.getCustomerName();
+            double amount = receipt.getreceiptAmount();
+            Console.WriteLine("{0} {1}", name, amount);
+            Console.ReadLine();
+        }
+    }
+}
